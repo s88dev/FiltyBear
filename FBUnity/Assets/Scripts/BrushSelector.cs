d77a6a1b45d24bb26cd351aa0e0e2a177ee01075
Michael Stephens <michael@s77.tv>
2014-11-24 12:11:14 -0500

    q
    q

---
+++ FBUnity/Assets/Scripts/BrushSelector.cs
@@ -0,0 +1,37 @@
+﻿
+
+
+using UnityEngine;
+using System.Collections;
+
+
+public class BrushSelector : MonoBehaviour 
+{
+	#region Variables
+
+
+
+	#endregion
+
+
+	#region Button Responses
+
+	// Selects the cleaning brush
+	public void CleaningBrushSelected ()
+	{
+		Debug.Log ("Cleaning Brush Selected!");
+	}
+
+	#endregion
+	
+
+	#region Initialization
+
+	// Used for initialization
+	void Start () 
+	{
+		
+	}
+
+	#endregion
+}
