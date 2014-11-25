4f17c33cb31d491caebf2b84fe65805ed7978b05
Michael Stephens <michael@s77.tv>
2014-11-25 15:18:58 -0500

    anim
    anim

--- FBUnity/Assets/Scripts/BrushSelector.cs
+++ FBUnity/Assets/Scripts/BrushSelector.cs
@@ -12,6 +12,18 @@ public class BrushSelector : MonoBehaviour
 
 	// The pain brush we are affecting
 	public D2D_ExplosionStamp brush;
+	// The bear gameObject we respawn sfter every reset
+	public GameObject bearPrefab;
+	// The possible background colors
+	public Color [] backgroundColors;
+	// The renderer for the baskground
+	public Renderer backgroundRend;
+	// The position at which we spawn the bear
+	private Vector3 _prefabSpawnPosition;
+	// The current brush size
+	private int _currentBrushSize;
+	// The current Bear Object
+	private bool _isInitialBear = true;
 
 	#endregion
 
@@ -27,13 +39,13 @@ public class BrushSelector : MonoBehaviour
 
 	//
 	//
-	public void BrushSizeChanged (int size)
+	public void BrushSizeChanged ()
 	{
-		switch (size)
+		switch (_currentBrushSize)
 		{
-			case 1: brush.Size = new Vector2 (0.25f, 0.25f); break;
-			case 2: brush.Size = new Vector2 (0.5f, 0.5f); break;
-			case 3: brush.Size = new Vector2 (1f, 1f); break;
+			case 1: _currentBrushSize = 2; brush.Size = new Vector2 (0.5f, 0.5f); break;
+			case 2: _currentBrushSize = 3; brush.Size = new Vector2 (1f, 1f); break;
+			case 3: _currentBrushSize = 1; brush.Size = new Vector2 (0.25f, 0.25f); break;
 		}
 	}
 
@@ -42,16 +54,35 @@ public class BrushSelector : MonoBehaviour
 	//
 	public void ResetButtonPressed ()
 	{
+		//
+		if (_isInitialBear)
+			Destroy (GameObject.Find ("Bear"));
+		else
+			Destroy (GameObject.Find ("Bear(Clone)"));
 
-	}
+		//
+		Instantiate (bearPrefab, _prefabSpawnPosition, Quaternion.identity);
 
-
-	//
-	//
-	public void CycleBackgroundButtonPressed ()
-	{
-		
+		//
+		_isInitialBear = false;
+		ChangeBackgroundColor ();
 	}
 
 	#endregion
+
+
+	//
+	void ChangeBackgroundColor ()
+	{
+		// Get a random color
+		Color col = backgroundColors [Random.Range (0, backgroundColors.Length)];
+		backgroundRend.material.color = col;
+	}
+
+
+	//
+	void Start ()
+	{
+		_prefabSpawnPosition = GameObject.Find ("Bear").transform.position;
+	}
 }
